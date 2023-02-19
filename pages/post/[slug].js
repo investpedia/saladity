8fc0b8e71310ad10ec1b6b6c0747387940d23a28
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-20 03:04:14 +0330

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import md from 'markdown-it';
 import Head from 'next/head';
-
+const BLOG_TITLE = "SOL BLOG";
 // dont change getStaticPaths() and getStaticProps()
 export async function getStaticPaths() {
   const files = fs.readdirSync('posts');
