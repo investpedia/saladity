73b0622c86b181f5dbeb2bd235b3d79f4d46e617
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-24 16:15:47 +0330

    Create global.js

---
+++ global.js
@@ -0,0 +1,16 @@
+// change dynamic information here 
+BLOG_TITLE =        "SOLIDITY STEPS";
+BLOG_DESCRIPTION =  "Solidity & Web3 from zero to hero";
+BLOG_MAIL =         "mailto=mosipvp@gmail.com";
+HOME =              "https://mosi-arch.github.io/saladity/";
+BLOG_DONATION =     "https://sol-app.github.io/Donation/";
+BLOG_SOL =          "https://sol-app.github.io/blog/";
+GITHUB =            "https://github.com/mosi-sol";
+LINKEDIN =          "https://www.linkedin.com/in/moslem-abbasi/";
+
+/*
+example:
+1-    import { BLOG_TITLE , BLOG_DONATION } from '../global';
+2-    const donat = global.BLOG_DONATION;
+3-    console.log(donat);
+*/
