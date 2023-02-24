7f6c22593091a28122023f5b2b3c7d4d7b2e287d
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-24 16:24:45 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import Layout from '../components/layout';
 import '../styles/globals.css';
+import NextScript from 'next/script';
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextScript
+            src='https://www.googletagmanager.com/gtag/js?id=G-change-me'
+            strategy='afterInteractive'
+          />
+          <NextScript id='google-analytics' strategy='afterInteractive'>
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-change-me');
+            `}
+          </NextScript>
+      
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
